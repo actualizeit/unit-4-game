b43acb683a583e9699e0d7431f1ec476eda6fa55
dbell <greenadvisor@gmail.com>
2019-07-22 13:10:57 -0400

    health display tied to current HP value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,22 @@
   
 $(document).ready(function() {
     heroPicked = false;
+    enemyAlive = false;
+    HPRemaining = this[0];
+    attackPower = this[1];
+    counterAttackPower = this[2];
+
+
+    //Stats: Health Points, Attack Power, Counter Attack Power
+    obiWanStats = [140,6,12]
+    lukeStats =  [120,8,14]
+    sidiousStats =  [150,6,10]
+    maulStats =  [180,8,8]
+
+    $(".obiWanHP").text(obiWanStats[0]);
+    $(".lukeHP").text(lukeStats[0]);
+    $(".sidiousHP").text(sidiousStats[0]);
+    $(".maulHP").text(maulStats[0]);
 
 
     $(".character").on("click", function() {
@@ -19,13 +35,14 @@ $(document).ready(function() {
         heroPicked = true;
         console.log(this);
     }else{
-        if($(this).hasClass("character")){
+        if($(this).hasClass("character") && enemyAlive == false){
         $(this).appendTo($("#defender"));
         $(this).removeClass('character');
         $(this).addClass('enemy');
+        enemyAlive = true;
         console.log(this);
-    }
-    }
+         }
+        }
     });
       
     
