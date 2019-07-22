fe13667cde7d26c06c2847555fc2c8510758dfc3
dbell <greenadvisor@gmail.com>
2019-07-22 12:31:22 -0400

    Defender append working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,27 @@
   
   
 $(document).ready(function() {
+    heroPicked = false;
+
+
     $(".character").on("click", function() {
+        console.log(heroPicked)
+        if(heroPicked == false){
         $(this).appendTo($("#yourCharacter"));
         $(".placeholder").css("display","none")
+        $(this).removeClass('character');
+        $(this).addClass('myGuy');
+        $(".character").appendTo($('#avalableEnemies'));
+        heroPicked = true;
+        console.log(this);
+    }else{
+        if($(this).hasClass("character")){
+        $(this).appendTo($("#defender"));
+        $(this).removeClass('character');
+        $(this).addClass('enemy');
+        console.log(this);
+    }
+    }
     });
       
     
