55ee4db56e8ee85f1c91cd9f080d02fa15fcb514
dbell <greenadvisor@gmail.com>
2019-07-21 23:30:25 -0400

    moving divs working

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+  
+  
+  
+  
+  
+$(document).ready(function() {
+    $(".character").on("click", function() {
+        $(this).appendTo($("#yourCharacter"));
+    });
+      
+    
+    
+    
+
+
+});
+
+// An object to hold the stats of each character:
+        //Health Points, Attack Power, and Counter Attack Power
