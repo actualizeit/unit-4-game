e227a9cfc0d3495198e4e37f2de8f5cc9db79e59
dbell <greenadvisor@gmail.com>
2019-07-22 23:35:51 -0400

    all basic game functions working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,16 +7,16 @@
 $(document).ready(function() {
     heroPicked = false;
     enemyAlive = false;
-    HPRemaining = this[0];
-    attackPower = this[1];
-    counterAttackPower = this[2];
-
 
     //Stats: Health Points, Attack Power, Counter Attack Power
     obiWanStats = [140,6,12]
     lukeStats =  [120,8,14]
     sidiousStats =  [150,6,10]
     maulStats =  [180,8,8]
+    myGuyStats = [];
+    enemyStats = [];
+    currentAttackPower = 0;
+    counter = 1;
 
     $(".obiWanHP").text(obiWanStats[0]);
     $(".lukeHP").text(lukeStats[0]);
@@ -33,17 +33,83 @@ $(document).ready(function() {
         $(this).addClass('myGuy');
         $(".character").appendTo($('#avalableEnemies'));
         heroPicked = true;
-        console.log(this);
+        if($(this).is('#obiWan')){
+            myGuyStats = obiWanStats
+        }
+        if($(this).is('#luke')){
+            myGuyStats = lukeStats
+        }
+        if($(this).is('#sidious')){
+            myGuyStats = sidiousStats
+        }
+        if($(this).is('#maul')){
+            myGuyStats = maulStats
+        }
+        console.log(myGuyStats);
     }else{
         if($(this).hasClass("character") && enemyAlive == false){
         $(this).appendTo($("#defender"));
         $(this).removeClass('character');
         $(this).addClass('enemy');
         enemyAlive = true;
-        console.log(this);
+        if($(this).is('#obiWan')){
+            enemyStats = obiWanStats
+        }
+        if($(this).is('#luke')){
+            enemyStats = lukeStats
+        }
+        if($(this).is('#sidious')){
+            enemyStats = sidiousStats
+        }
+        if($(this).is('#maul')){
+            enemyStats = maulStats
+        }
+        console.log(enemyStats);
          }
         }
     });
+
+    $("#attackButton").on("click", function() {
+        if(heroPicked == true && enemyAlive == true){
+            currentAttackPower = myGuyStats[1] * counter;
+            enemyStats[0] = enemyStats[0] - currentAttackPower;
+            myGuyStats[0] = myGuyStats[0] - enemyStats[2];
+            counter++;
+            if($(".myGuy").is('#obiWan')){
+                $(".obiWanHP").text(myGuyStats[0]);
+            }
+            if($(".myGuy").is('#luke')){
+                $(".lukeHP").text(myGuyStats[0]);
+            }
+            if($(".myGuy").is('#sidious')){
+                $(".sidiousHP").text(myGuyStats[0]);
+            }
+            if($(".myGuy").is('#maul')){
+                $(".maulHP").text(myGuyStats[0]);
+            }
+            if($(".enemy").is('#obiWan')){
+                $(".obiWanHP").text(enemyStats[0]);
+            }
+            if($(".enemy").is('#luke')){
+                $(".lukeHP").text(enemyStats[0]);
+            }
+            if($(".enemy").is('#sidious')){
+                $(".sidiousHP").text(enemyStats[0]);
+            }
+            if($(".enemy").is('#maul')){
+                $(".maulHP").text(enemyStats[0]);
+            }
+            if(enemyStats[0] < 1){
+                enemyAlive = false;
+                $(".enemy").hide();
+            }
+            if(myGuyStats[0] < 1){
+                
+            }
+        }
+    });
+
+
       
     
     
